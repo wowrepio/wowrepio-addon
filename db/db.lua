2c5e3d7e9f885c111e7b08c04bba30206a3dc1a0
Jasiek Matusz <me@marahin.pl>
2021-03-12 20:30:57 +0100

    remove placeholder for Mararcher

--- db/db.lua
+++ db/db.lua
@@ -4,5 +4,4 @@ ns.DATABASE = {}
 
 -- HERE ARE DRAGONS --
 
-ns.DATABASE["eu/burning-legion/Sakreble"] = { average = 5.0, factors = { skill = 1.5, teamplay = 3.2, communication = 0.8 } }
-ns.DATABASE["eu/burning-legion/Mararcher"] = { average = 5.0, factors = { skill = 1.5, teamplay = 3.2, communication = 0.8 } }
+ns.DATABASE["eu/burning-legion/Sakreble"] = { average = 1.83, factors = { skill = 1.5, teamplay = 3.2, communication = 0.8 } }
