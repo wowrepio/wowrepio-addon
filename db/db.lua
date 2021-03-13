5dfb539faff6f62d8baff804757842b4425bc4df
Jasiek Matusz <me@marahin.pl>
2021-03-13 16:03:11 +0100

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -3,4 +3,16 @@ local _, ns = ...
 ns.DATABASE = {}
 
 -- HERE ARE DRAGONS --
-ns.DATABASE["eu/burning-legion/Renkawdziure"] = { average = 2.33, factors = { skill = 1.00, teamplay = 3.00, communication = 3.00 } }
+ns.DATABASE["eu/burning-legion/Sakreble"] = { average = 3.00, factors = { skill = 2.33, teamplay = 3.33, communication = 3.33 } }
+
+ns.DATABASE["eu/draenor/Chuffi"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
+
+ns.DATABASE["eu/argent-dawn/Agatha"] = { average = 1.67, factors = { skill = 1.00, teamplay = 2.00, communication = 2.00 } }
+
+ns.DATABASE["eu/burning-legion/Renkawdziure"] = { average = 3.00, factors = { skill = 5.00, teamplay = 2.00, communication = 2.00 } }
+
+ns.DATABASE["eu/argent-dawn/Rafikim"] = { average = 2.67, factors = { skill = 4.00, teamplay = 2.00, communication = 2.00 } }
+
+ns.DATABASE["eu/burning-legion/Cowgoesmuu"] = { average = 4.00, factors = { skill = 4.00, teamplay = 4.00, communication = 4.00 } }
+
+ns.DATABASE["eu/draenor/Cycozuj"] = { average = 4.33, factors = { skill = 5.00, teamplay = 4.00, communication = 4.00 } }
--- tools/refresh_data.rb
+++ tools/refresh_data.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 # github_api_token = ENV.fetch('GITHUB_API_TOKEN')
 wowrepio_api_token = ENV.fetch('WOWREPIO_API_TOKEN')
 
-data = JSON.parse(URI.open('http://localhost:3000/api/scores?token=' + wowrepio_api_token).read)
+data = JSON.parse(URI.open('https://wowrep.io/api/scores?token=' + wowrepio_api_token).read)
 lua_template_line = 'ns.DATABASE["%KEY%"] = { average = %AVERAGE%, factors = { skill = %SKILL%, teamplay = %TEAMPLAY%, communication = %COM% } }'
 to_be_lua_lines = {}
 lua_lines = []
