d0a3bf1271e83350f88a2ce95f05e6dec10e6093
marahin <me@marahin.pl>
2021-03-15 23:00:08 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -36,3 +36,4 @@ ns.DATABASE[937598863] = { average = 5.00, factors = { skill = 5.00, teamplay = 
 ns.DATABASE[594317957] = { average = 2.33, factors = { skill = 2.00, teamplay = 1.00, communication = 4.00 } }
 ns.DATABASE[431292913] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE[96613610] = { average = 3.67, factors = { skill = 4.00, teamplay = 4.00, communication = 3.00 } }
+ns.DATABASE[342577321] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
