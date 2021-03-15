c23b39a3777cae2f24692ad3b136ae9dca98cb67
marahin <me@marahin.pl>
2021-03-15 05:00:13 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -14,6 +14,7 @@ ns.DATABASE["eu/hyjal/ßounty"] = { average = 5.00, factors = { skill = 5.00, te
 ns.DATABASE["eu/twisting-nether/Miserére"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/soulflayer/Дихгал"] = { average = 4.17, factors = { skill = 4.00, teamplay = 3.50, communication = 5.00 } }
 ns.DATABASE["eu/draenor/Cycozuj"] = { average = 4.67, factors = { skill = 5.00, teamplay = 5.00, communication = 4.00 } }
+ns.DATABASE["eu/argent-dawn/Deanyra"] = { average = 4.33, factors = { skill = 3.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/burning-legion/Renkawdziure"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/nemesis/Parsyvall"] = { average = 2.33, factors = { skill = 1.00, teamplay = 3.00, communication = 3.00 } }
 ns.DATABASE["eu/burning-legion/Nekosa"] = { average = 4.33, factors = { skill = 5.00, teamplay = 5.00, communication = 3.00 } }
