b9c03c5b6980c87c205df27153119f914f2a4b02
marahin <me@marahin.pl>
2021-03-15 13:00:18 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -16,9 +16,13 @@ ns.DATABASE["eu/soulflayer/Дихгал"] = { average = 4.17, factors = { skill 
 ns.DATABASE["eu/draenor/Cycozuj"] = { average = 4.67, factors = { skill = 5.00, teamplay = 5.00, communication = 4.00 } }
 ns.DATABASE["eu/argent-dawn/Deanyra"] = { average = 4.33, factors = { skill = 3.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/burning-legion/Renkawdziure"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
+ns.DATABASE["eu/draenor/Boneshocky"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
+ns.DATABASE["eu/draenor/Fayntix"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/nemesis/Parsyvall"] = { average = 2.33, factors = { skill = 1.00, teamplay = 3.00, communication = 3.00 } }
 ns.DATABASE["eu/burning-legion/Nekosa"] = { average = 4.33, factors = { skill = 5.00, teamplay = 5.00, communication = 3.00 } }
 ns.DATABASE["eu/burning-legion/Kaeri"] = { average = 4.33, factors = { skill = 5.00, teamplay = 5.00, communication = 3.00 } }
 ns.DATABASE["eu/burning-legion/Toonik"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/draenor/Ahanna"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
 ns.DATABASE["eu/stormscale/Freddee"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
+ns.DATABASE["eu/draenor/Carbzilla"] = { average = 3.67, factors = { skill = 1.00, teamplay = 5.00, communication = 5.00 } }
+ns.DATABASE["eu/draenor/Qminator"] = { average = 5.00, factors = { skill = 5.00, teamplay = 5.00, communication = 5.00 } }
