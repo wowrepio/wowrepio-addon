c37e8266a468ab2a6fae03676ec9f14ab3d16243
marahin <me@marahin.pl>
2021-03-31 20:00:05 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -5,4 +5,5 @@ ns.DATABASE = {}
 -- HERE ARE DRAGONS --
 ns.DATABASE[426948687] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[44835056] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
-ns.DATABASE[371043048] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
+ns.DATABASE[997306609] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
+ns.DATABASE[722406898] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
