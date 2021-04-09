7421f0d12d699e5e1324ff8ccdae86879e16e0f6
marahin <me@marahin.pl>
2021-04-09 18:00:09 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -23,7 +23,8 @@ ns.DATABASE[184383607] = { average = 3.61, factors = { skill = 3.55, teamplay = 
 ns.DATABASE[7530813] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[286209706] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
 ns.DATABASE[426172703] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
-ns.DATABASE[878155062] = { average = 3.62, factors = { skill = 3.62, teamplay = 3.69, communication = 3.54 } }
+ns.DATABASE[878155062] = { average = 3.59, factors = { skill = 3.62, teamplay = 3.62, communication = 3.54 } }
+ns.DATABASE[121139353] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
 ns.DATABASE[890569930] = { average = 3.69, factors = { skill = 3.69, teamplay = 3.69, communication = 3.69 } }
 ns.DATABASE[162297781] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[757256289] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
