c7cf2bfd3fec86eaba77b2d6f111aa2ab846e0f0
marahin <me@marahin.pl>
2021-04-19 23:00:33 +0000

    Update db.lua

--- db/db.lua
+++ db/db.lua
@@ -674,6 +674,7 @@ ns.DATABASE[282482075] = { average = 3.64, factors = { skill = 3.75, teamplay = 
 ns.DATABASE[148312517] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[823324693] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[326205091] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
+ns.DATABASE[87799336] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
 ns.DATABASE[904684769] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
 ns.DATABASE[148312392] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[240617239] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
@@ -726,3 +727,4 @@ ns.DATABASE[155730647] = { average = 3.75, factors = { skill = 3.75, teamplay = 
 ns.DATABASE[788356873] = { average = 3.75, factors = { skill = 3.75, teamplay = 3.75, communication = 3.75 } }
 ns.DATABASE[247038248] = { average = 3.54, factors = { skill = 3.55, teamplay = 3.64, communication = 3.45 } }
 ns.DATABASE[993663366] = { average = 3.58, factors = { skill = 3.58, teamplay = 3.58, communication = 3.58 } }
+ns.DATABASE[916864729] = { average = 3.64, factors = { skill = 3.64, teamplay = 3.64, communication = 3.64 } }
